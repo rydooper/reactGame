e6aa4d3ab89f8f51ed6572ad2abde241b1568042
Ryder <ryderarenfranklin@gmail.com>
2021-03-20 18:39:21 +0000

    implemented history of board!

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -56,12 +56,13 @@ class Game extends React.Component {
             history: [{
                 squares: Array(9).fill(null),
             }],
+            stepNumber: 0,
             xIsNext: true,
         };
     } //game has history as the oldest parent (not sure how else to say top of the parent tier)
     
     handleClick(i) {
-        const history = this.state.history;
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length -1];
         const squares = current.squares.slice();
         if (calculateWinner(squares) || squares[i]) {
@@ -72,16 +73,37 @@ class Game extends React.Component {
             hsitory: history.concat([{
                 squares: squares,
             }]),
+            stepNumber: history.length,
             xIsNext: !this.state.xIsNext,
         }); 
     } //sets value in square to O or X depending on xIsNext state (true=X, false=Y)
       //also tracks history of moves
 
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0,
+        });
+    }
 
     render() {
         const history = this.state.history;
-        const current = history[history.length -1];
+        const current = history[this.state.stepNumber];
         const winner = calculateWinner(current.squares);
+        
+        const moves = history.map((step, move) => {
+            const desc = move ?
+                'Go to move #' + move:
+                'Go to game start';
+            return (
+                <li key={move}>
+                    <button onClick={() => this.jumpTo(move)}>{desc}
+                    </button>
+                </li>
+            );
+        });
+        
+        
         let status;
 
         if(winner) {
@@ -100,7 +122,7 @@ class Game extends React.Component {
                 </div>
                 <div className="game-info">
                     <div> {status} </div>
-                    <ol> {/* todo */} </ol>
+                    <ol> {moves} </ol>
                 </div>
             </div>
         );
